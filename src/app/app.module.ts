df680b46a7976836068dde62a0757a5d86c03cb9
Kelsen Hazelwood <khazelwood@dminc.com>
2019-01-24 16:14:28 -0500

    Fixed app module conflicts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderModule } from './components/header/header.module';
-import { MainModule } from './components/main/main.module'
 
-import { CoreModule } from './services/core_module';
+import { AppRoutingModule } from 'app/app-routing.module';
+import { AppComponent } from 'app/app.component';
+import { HeaderModule } from 'app/components/header/header.component';
+import { MainModule } from 'app/components/main/main.module'
+
+
+import { CoreModule } from 'app/services/core_module';
+import { BookCreateComponent } from 'app/components/book-create/book-create.component';
 
 @NgModule({
   declarations: [
     AppComponent
+    BookCreateComponent,
   ],
   imports: [
     BrowserModule,
