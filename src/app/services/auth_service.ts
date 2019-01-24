eaeeb9177403416b7e6a6b54451edd0dd9d24abc
Jake Berg <jberg@dminc.com>
2019-01-24 11:49:13 -0500

    Changed relative import for users, changed endpoints

--- src/app/services/auth_service.ts
+++ src/app/services/auth_service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { User } from '../../interfaces/user';
+import User from '../../interfaces/user';
 import { Observable } from 'rxjs';
 
 @Injectable()
@@ -15,7 +15,7 @@ export class AuthService {
     })
   }
 
-  authPath = "http://localhost:3000/login";
+  authPath = "http://localhost:3000/users/login";
 
   // Returns an observable
   authenticate(user: User): Observable<{}> {
